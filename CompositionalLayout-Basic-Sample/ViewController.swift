e93239e9228791f5871ae4d4686868adb94a9fc6
Dai Kawahara <skbsporta7@gmail.com>
2020-01-20 20:43:33 +0900

    Initial commit

---
+++ CompositionalLayout-Basic-Sample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CompositionalLayout-Basic-Sample
+//
+//  Created by kawaharadai on 2020/01/20.
+//  Copyright © 2020 kawaharadai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
